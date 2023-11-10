5c0440eea3f63037aba4786c57edea9451ee51b4
ReplikaFLEX <replikainside@gmail.com>
2023-11-11 04:10:25 +0700

    Replika

--- app/src/main/java/com/example/coffeecup/login_page.java
+++ app/src/main/java/com/example/coffeecup/login_page.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class login_page extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.registration_page);
+        setContentView(R.layout.activity_login_page);
     }
 }
